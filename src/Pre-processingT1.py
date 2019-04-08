7def037dcbb6e6824872d5928e41c8ce44c419fc
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2019-04-08 07:50:15 -0400

    Update Pre-processingT1.py

--- src/Pre-processingT1.py
+++ src/Pre-processingT1.py
@@ -1,37 +1,60 @@
-from __future__ import division
-import os
-import sys
 import numpy as np
 import nibabel as nib
-import cv2 as cv
 
-cases = 'case.txt'
-binary_file = 'output/binary'
-training_data='output/data.npy'
+def normalize_data(data, mean, std):
+    print("Mean = ", mean)
+    print("SD = ", std)
+    data -= mean
+    data /= std
+    return data
 
+binary_file = '/rfanfs/pnl-zorro/home/sq566/OASIS/X_train/output/binary'
+training_data='/rfanfs/pnl-zorro/home/sq566/OASIS/X_train/output/data.npy'
+
+def normalize_data_storage(data_storage):
+    means = list()
+    stds = list()
+    for subject in case_arr:
+        img = nib.load(subject)
+        imgU16 = img.get_data().astype(np.int16)
+        means.append(imgU16.mean(axis=(0, 1, 2)))
+        stds.append(imgU16.std(axis=(0, 1, 2)))
+    mean = np.asarray(means).mean(axis=0)
+    std = np.asarray(stds).mean(axis=0)
+    f_handle = open(binary_file, 'wb')
+    count = 0
+    for subject in case_arr:
+        print("Normalizing ", subject)
+        img = nib.load(subject)
+        data = img.get_data().astype(np.float64)
+        data_n = normalize_data(data, mean, std)
+        data_n[data_n < 0.0] = 0;
+        data.tofile(f_handle)
+        print('Case ' + str(count) + ' done')
+        count = count + 1
+    f_handle.close()
+
+
+cases = '/rfanfs/pnl-zorro/home/sq566/OASIS/X_train/case.txt'
 with open(cases) as f:
     case_arr = f.read().splitlines()
+
+normalize_data_storage(case_arr)
 
 x_dim=176
 y_dim=208
 z_dim=176
 total_case = len(case_arr)
-count=0
-
-f_handle = open(binary_file, 'wb')
-for subjects in case_arr:
-    img = nib.load(subjects)
-    imgU16 = img.get_data().astype(np.int16)    # Signed 16-bit Integer (-32768 to 32767)
-    imgU16 = imgU16.reshape(x_dim, y_dim, z_dim)
-    data = imgU16 / 32767.0
-    data.tofile(f_handle)
-    print 'Case ' + str(count) + ' done'
-    count = count + 1
-f_handle.close()
-
 merge = np.memmap(binary_file, dtype=np.float64, mode='r+', shape=(x_dim*total_case, y_dim, z_dim))
-print merge.shape
-print type(merge)
+print(merge.shape)
 
 np.save(training_data, merge)
-	
+
+
+#import pickle
+#
+#with open('mean', 'wb') as fp:
+#    pickle.dump(means, fp)
+#    
+#with open('std', 'wb') as fp:
+#    pickle.dump(stds, fp)

2d19d3c89080fa8e77175ec3538c6cdb08a259d3
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2019-05-17 12:10:32 -0400

    Update plot.py

--- src/plot.py
+++ src/plot.py
@@ -8,21 +8,16 @@ for line in f:
     
 column_header = data_header
 data_pandas = pandas.read_csv('DWIlog_99percentile.csv', sep = ';', names=column_header)
-
 SO_loss = list(map(float, data_pandas.final_op_loss.tolist()[1:]))
-SO_loss = [ abs(x) for x in SO_loss]
 
 L = len(SO_loss)
 plt.xlim(0, L - 1)
 plt.ylim(0, 1)
 
-x = []
-for i in range(0, L):
-    x.append(i)
+X = list(range(0, L))
+Y = [abs(x) for x in SO_loss] 
 
-plt.plot(x, SO_loss, marker='o', markerfacecolor='red', color='blue', linestyle=':');
-
-
+plt.plot(X, Y, marker='o', markerfacecolor='red', color='blue', linestyle=':');
 plt.xlabel('No of epochs', size=12)
 plt.ylabel('Dice Coefficient', size=12)   
 plt.grid(linestyle='dotted')

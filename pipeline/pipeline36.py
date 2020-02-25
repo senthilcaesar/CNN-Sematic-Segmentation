ea43aff4db35616efb0e65e40037747c889fb601
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2020-02-25 14:20:38 -0500

    Update pipeline36.py

--- pipeline/pipeline36.py
+++ pipeline/pipeline36.py
@@ -2,7 +2,7 @@ from __future__ import division
 # -----------------------------------------------------------------
 # Author:       Senthil Palanivelu, Tashrif Billah                 
 # Written:      01/22/2020                             
-# Last Updated:     02/12/2020
+# Last Updated:     02/25/2020
 # Purpose:          Pipeline for diffusion brain masking
 # -----------------------------------------------------------------
 
@@ -507,9 +507,6 @@ if __name__ == '__main__':
     parser.add_argument('-i', action='store', dest='dwi', type=str,
                         help=" input caselist file in txt format")
 
-    parser.add_argument('-ref', action='store', dest='ref', type=str,
-                        help=" reference b0 file for registration")
-
     parser.add_argument('-f', action='store', dest='model_folder', type=str,
                         help=" folder which contain the trained model")
 
@@ -558,7 +555,8 @@ if __name__ == '__main__':
             #print(unique)
             storage = os.path.dirname(case_arr[0])
             tmp_path = storage + '/'
-            reference = str(args.ref)
+            trained_model_folder = args.model_folder.rstrip('/')
+            reference = trained_model_folder + '/IITmean_b0_256.nii.gz'
 
             binary_file_s = storage + '/' + unique + '_binary_s'
             binary_file_c = storage + '/'+ unique + '_binary_c'
@@ -672,7 +670,6 @@ if __name__ == '__main__':
             print ("Pre-Processing Time Taken : ", round(int(total_preprocessing_time.seconds)/60, 2), " min")
 
             # DWI Deep Learning Segmentation
-            trained_model_folder = args.model_folder.rstrip('/')
             dwi_mask_sagittal = predict_mask(cases_file_s, trained_model_folder, view='sagittal')
             dwi_mask_coronal = predict_mask(cases_file_c, trained_model_folder, view='coronal')
             dwi_mask_axial = predict_mask(cases_file_a, trained_model_folder, view='axial')

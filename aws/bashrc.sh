e94d6e1c49f5888a41ef1153080ce7d96701e53d
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2020-04-01 16:46:15 -0400

    Create bashrc.sh

---
+++ aws/bashrc.sh
@@ -0,0 +1,36 @@
+source /rfanfs/pnl-zorro/software/bashrc
+#source /rfanfs/pnl-zorro/software/pnlpipe3/bashrc3
+#source /mnt/data0/build/miniconda3/bin/activate
+#source /rfanfs/pnl-zorro/home/suheyla/Tools/tensorflow-prebuiltin-pycharm/bin/activate
+#source /rfanfs/pnl-zorro/home/sq566/Downloads/conda/etc/profile.d/conda.sh
+#Python3
+#source /rfanfs/pnl-zorro/home/sq566/Downloads/conda/bin/activate
+# Python 2.7 senthil deep learning
+#source /rfanfs/pnl-zorro/home/sq566/pycharm/yellow/bin/activate
+alias charm='/rfanfs/pnl-zorro/home/suheyla/Tools/pycharm-community-2018.1.2/bin/pycharm.sh'
+alias python3='/rfanfs/pnl-zorro/home/sq566/Downloads/conda/bin/python'
+alias spyder='/rfanfs/pnl-zorro/home/sq566/Downloads/conda/bin/spyder'
+alias piper='ssh sq566@piper.nmr.mgh.harvard.edu'
+alias eris='ssh sq566@eris1n2.research.partners.org'
+alias pnl-Yogesh='ssh sq566@170.223.221.160'
+alias pnl-Lipeng='ssh sq566@pnl-maxwell'
+alias pnl='ssh sq566@pnl-t55-6.bwh.harvard.edu'
+alias niv='nvidia-smi -l 1'
+alias suite='/rfanfs/pnl-zorro/home/sq566/brainsuite/BrainSuite19a/bin/BrainSuite19a'
+alias visa='/rfanfs/pnl-zorro/home/sq566/brainvisa/BrainVISA'
+alias home='cd /rfanfs/pnl-zorro/home/sq566'
+export PATH="/home/sq566/bin:$PATH"
+export PATH="/rfanfs/pnl-zorro/home/sq566/CompNetPipeline:$PATH"
+
+export PATH=$PATH:/rfanfs/pnl-zorro/home/sq566/Downloads/conversion/conversion:/rfanfs/pnl-zorro/home/sq566/vscode/VSCode-linux-x64/bin
+export VIMRUNTIME=/home/sq566/usr/local/share/vim/vim81/
+#alias vim='/home/sq566/.local/usr/local/bin/vim'
+alias manager='/rfanfs/pnl-zorro/projects/Harmonization/tools/sqldeveloper/jre1.8.0_221/bin/javaws /rfanfs/pnl-zorro/projects/Harmonization/tools/sqldeveloper/DownloadManager.jnlp'
+alias java='/rfanfs/pnl-zorro/projects/Harmonization/tools/sqldeveloper/jre1.8.0_221/bin/java'
+alias sqldev='/rfanfs/pnl-zorro/projects/Harmonization/tools/sqldeveloper/opt/sqldeveloper/sqldeveloper.sh'
+alias s410='/rfanfs/pnl-zorro/home/sq566/Downloads/slicer-4.10.2/Slicer-4.10.2-linux-amd64/Slicer'
+alias sublime='/rfanfs/pnl-zorro/home/sq566/sublime/sublime_text_3/sublime_text'
+alias chrome='/rfanfs/pnl-zorro/home/sq566/Downloads/chrome/opt/google/chrome/chrome --no-sandbox'
+alias dicom_to_nii='/rfanfs/pnl-zorro/software/pnlpipe3/dcm2niix/build/bin/dcm2niix'
+alias eddy_sq='/rfanfs/pnl-zorro/home/sq566/kevin/eddy-squeeze/eddy_squeeze/eddy_squeeze'
+ln -s /rfanfs/pnl-zorro/home/sq566/vscode/VSCode-linux-x64/bin ~/bin/
